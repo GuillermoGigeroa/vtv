d276f1deef522540b1e89057e1e76d37b7c2f88f
Guillermo Gigeroa <guillermo.gigeroa@hotmail.com>
2021-12-10 12:32:25 -0300

    Se crea clase para conexión a BBDD con su interface

---
+++ src/main/java/com/gigeroa/vtv/repositories/IConexion.java
@@ -0,0 +1,14 @@
+package com.gigeroa.vtv.repositories;
+
+/*
+ *	Interfaz creada para poder cargar los datos de la
+ *	BBDD que necesitamos conectar al proyecto.
+ */
+
+public interface IConexion {
+	final String host = "localhost/";
+	final String user = "root";
+	final String pass = "root";
+	final String dbName = "vtv";
+	final String url = "jdbc:mysql://" + host + dbName;
+}
