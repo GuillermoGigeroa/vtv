bb1563b521f309b8cb4427709013bc84c0ce820a
Guillermo Gigeroa <guillermo.gigeroa@hotmail.com>
2021-12-10 11:51:37 -0300

    Se agrega clase vehículo con sus propiedades y métodos

---
+++ src/main/java/com/gigeroa/vtv/entities/Vehiculo.java
@@ -0,0 +1,82 @@
+package com.gigeroa.vtv.entities;
+
+import com.gigeroa.vtv.repositories.*;
+
+public class Vehiculo implements IContieneID {
+	private int id;
+	private String dominio;
+	private String marca;
+	private String modelo;
+	private Propietario propietario;
+	
+//	Constructores
+	public Vehiculo() {
+		this.id = -1;
+		this.dominio = "Dominio";
+		this.marca = "Marca";
+		this.modelo = "Modelo";
+		this.propietario = new Propietario();
+	}
+	
+	public Vehiculo(String dominio, String marca, String modelo, Propietario propietario) {
+		this.id = -1;
+		this.dominio = dominio;
+		this.marca = marca;
+		this.modelo = modelo;
+		this.propietario = propietario;
+	}
+	
+	public Vehiculo(int id, String dominio, String marca, String modelo, Propietario propietario) {
+		this.id = id;
+		this.dominio = dominio;
+		this.marca = marca;
+		this.modelo = modelo;
+		this.propietario = propietario;
+	}
+
+//	Métodos
+	public String getDominio() {
+		return dominio;
+	}
+
+	public void setDominio(String dominio) {
+		this.dominio = dominio;
+	}
+
+	public String getMarca() {
+		return marca;
+	}
+
+	public void setMarca(String marca) {
+		this.marca = marca;
+	}
+
+	public String getModelo() {
+		return modelo;
+	}
+
+	public void setModelo(String modelo) {
+		this.modelo = modelo;
+	}
+
+	public Propietario getPropietario() {
+		return propietario;
+	}
+
+	public void setPropietario(Propietario propietario) {
+		this.propietario = propietario;
+	}
+	
+	public int getID() {
+		return id;
+	}
+	
+	public void setID(int id) {
+		this.id = id;
+	}
+	
+	@Override
+	public String toString() {
+		return marca + " - " + modelo + " - " + dominio;
+	}
+}
