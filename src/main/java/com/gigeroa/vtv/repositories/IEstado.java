0d424e0f421020da5717be74d1e40a87528c69d4
Guillermo Gigeroa <guillermo.gigeroa@hotmail.com>
2021-12-10 16:56:04 -0300

    Comentarios
    Interfaz creada para administrar los estados

--- src/main/java/com/gigeroa/vtv/repositories/IEstado.java
+++ src/main/java/com/gigeroa/vtv/repositories/IEstado.java
@@ -1,5 +1,9 @@
 package com.gigeroa.vtv.repositories;
 
+/*
+ *	Interfaz creada para administrar los estados
+ */
+
 public interface IEstado {
 	final String apto = "Apto";
 	final String condicional = "Condicional";
