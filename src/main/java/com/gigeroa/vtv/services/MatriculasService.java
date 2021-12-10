c52dceb9b5d016f14292898cd5b28f5160347988
Guillermo Gigeroa <guillermo.gigeroa@hotmail.com>
2021-12-10 17:56:38 -0300

    Se agrega procesarMatricula y esNueva
    procesarMatricula - Método para procesar el texto ingresado y verificar si es válido
    esNueva - Método para identificar si la matrícula ingresada es formato antiguo o nuevo

--- src/main/java/com/gigeroa/vtv/services/MatriculasService.java
+++ src/main/java/com/gigeroa/vtv/services/MatriculasService.java
@@ -5,10 +5,37 @@ package com.gigeroa.vtv.services;
  */
 
 public abstract class MatriculasService {
-	public static void verificarNuevaMatricula (String matricula) {
-		String matriculaActual = matricula.toLowerCase().trim();
-		for (int x = 0; x < matriculaActual.length(); x++) {
-			
+	
+//	Método para procesar el texto ingresado y verificar si es válido
+	public boolean procesarMatricula (String matricula) {
+		String matriculaActual = matricula.toUpperCase().trim();
+		// TODO verificar longitud maxima y mínima
+		if (esNueva(matriculaActual)) {
+//			Código para matrícula nueva
+			return true;
+		}
+		else {
+//			Código para matrícula antigua
+			return false;
+		}
+	}
+	
+//	Método para identificar si la matrícula ingresada es formato antiguo o nuevo
+	public static boolean esNueva (String matricula) {
+		/*	
+		 * Se verifica el tercer caracter ya que 
+		 * al tener una matrícula "AA123AA"
+		 * el tercer caracter es un número, pero
+		 * en cambio en "AAA123" el tercer caracter
+		 * es una letra, y esto desencadena una
+		 * NumberFormatException.
+		 */
+		try {
+			Integer.valueOf(matricula.charAt(2));
+			return true;
+		}
+		catch (NumberFormatException error) {
+			return false;
 		}
 	}
 }
