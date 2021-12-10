4f1429b7f13a52fd8dc4278af6f51890b29d77bd
Guillermo Gigeroa <guillermo.gigeroa@hotmail.com>
2021-12-10 17:58:18 -0300

    Lista TODO para continuar

--- src/main/java/com/gigeroa/vtv/repositories/IMatricula.java
+++ src/main/java/com/gigeroa/vtv/repositories/IMatricula.java
@@ -1,6 +1,7 @@
 package com.gigeroa.vtv.repositories;
 
 public interface IMatricula {
+//	TODO Ingresar maximo y mínimo de longitud del String a recibir
 	String sinMatricula = "Sin matricula";
 	void setMatricula(String matricula);
 	String getMatricula();
