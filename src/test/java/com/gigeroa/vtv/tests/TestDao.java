a3cbb6745daf7fc2a4567fe84bc1fd12f7d41161
Guillermo Gigeroa <guillermo.gigeroa@hotmail.com>
2021-12-10 14:28:26 -0300

    Se trae la dependencia de JUnit y se generan unos casos de prueba

---
+++ src/test/java/com/gigeroa/vtv/tests/TestDao.java
@@ -0,0 +1,48 @@
+package com.gigeroa.vtv.tests;
+
+import com.gigeroa.vtv.dao.DaoInspeccion;
+import com.gigeroa.vtv.dao.DaoInspectores;
+import com.gigeroa.vtv.dao.DaoPropietarios;
+import com.gigeroa.vtv.dao.DaoVehiculos;
+
+import junit.framework.TestCase;
+
+public class TestDao extends TestCase{
+	private DaoInspeccion daoInspeccion;
+	private DaoInspectores daoInspectores;
+	private DaoPropietarios daoPropietarios;
+	private DaoVehiculos daoVehiculos;
+	
+	public void escenarioInspeccion () {
+		daoInspeccion = new DaoInspeccion();
+	}
+	public void escenarioInspectores () {
+		daoInspectores = new DaoInspectores();
+	}
+	public void escenarioPropietarios () {
+		daoPropietarios = new DaoPropietarios();
+	}
+	public void escenarioVehiculos () {
+		daoVehiculos = new DaoVehiculos();
+	}
+	
+	public void testListarInspecciones () {
+		escenarioInspeccion();
+		assertNotNull(daoInspeccion.listarInspecciones());
+	}
+	
+	public void testListarPropietarios () {
+		escenarioPropietarios();
+		assertNotNull(daoPropietarios.listarPropietarios());
+	}
+	
+	public void testListarInspectores () {
+		escenarioInspectores();
+		assertNotNull(daoInspectores.listarInspectores());
+	}
+	
+	public void testListarVehiculos () {
+		escenarioVehiculos();
+		assertNotNull(daoVehiculos.listarVehiculos());
+	}
+}
