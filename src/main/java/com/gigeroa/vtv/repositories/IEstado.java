73055b9a3c90d7e23dd06c37d18a943b26b9b623
Guillermo Gigeroa <guillermo.gigeroa@hotmail.com>
2021-12-10 12:34:39 -0300

    Actualización de IEstado sacando public static al no ser necesario

--- src/main/java/com/gigeroa/vtv/repositories/IEstado.java
+++ src/main/java/com/gigeroa/vtv/repositories/IEstado.java
@@ -1,8 +1,8 @@
 package com.gigeroa.vtv.repositories;
 
 public interface IEstado {
-	public static final String apto = "Apto";
-	public static final String condicional = "Condicional";
-	public static final String rechazado = "Rechazado";
-	public static final String error = "Error";
+	final String apto = "Apto";
+	final String condicional = "Condicional";
+	final String rechazado = "Rechazado";
+	final String error = "Error";
 }
