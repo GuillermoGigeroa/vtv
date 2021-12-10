5e6cd15b581a1bce0c85a746990110ec5930f658
Guillermo Gigeroa <guillermo.gigeroa@hotmail.com>
2021-12-10 17:20:34 -0300

    Preparando el método para analizar si es nueva matrícula

--- src/main/java/com/gigeroa/vtv/services/MatriculasService.java
+++ src/main/java/com/gigeroa/vtv/services/MatriculasService.java
@@ -5,5 +5,10 @@ package com.gigeroa.vtv.services;
  */
 
 public abstract class MatriculasService {
-	
+	public static void verificarNuevaMatricula (String matricula) {
+		String matriculaActual = matricula.toLowerCase().trim();
+		for (int x = 0; x < matriculaActual.length(); x++) {
+			
+		}
+	}
 }
