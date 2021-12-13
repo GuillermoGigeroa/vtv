f4c39093f4b3bc948c603fe15b7c0520f3f42805
Guillermo Gigeroa <guillermo.gigeroa@hotmail.com>
2021-12-13 09:12:18 -0300

    Update MatriculasService
    Se agregan funciones para detectar si es formato antiguo o nuevo, luego se revisará los caracteres restantes en otras funciones

--- src/main/java/com/gigeroa/vtv/services/MatriculasService.java
+++ src/main/java/com/gigeroa/vtv/services/MatriculasService.java
@@ -1,5 +1,8 @@
 package com.gigeroa.vtv.services;
 
+import com.gigeroa.vtv.entities.Matricula;
+import com.gigeroa.vtv.repositories.IMatricula;
+
 /*
  * Servicio creado para poder administrar las validaciones.
  */
@@ -7,31 +10,47 @@ package com.gigeroa.vtv.services;
 public abstract class MatriculasService {
 	
 //	Método para procesar el texto ingresado y verificar si es válido
-	public boolean procesarMatricula (String matricula) {
+	public IMatricula procesarMatricula (String matricula) {
+		IMatricula resultado = null;
 		String matriculaActual = matricula.toUpperCase().trim();
-		// TODO verificar longitud maxima y mínima
-		if (esNueva(matriculaActual)) {
-//			Código para matrícula nueva
-			return true;
+		if (!(matriculaActual.length() >= 6 && matriculaActual.length() <= 7)) {
+			return null;
 		}
-		else {
-//			Código para matrícula antigua
-			return false;
+		if (esNueva(matriculaActual) || esAntigua(matriculaActual)) {
+			resultado = new Matricula(matriculaActual);
 		}
+		return resultado;
 	}
 	
-//	Método para identificar si la matrícula ingresada es formato antiguo o nuevo
+//	Método para identificar si la matrícula ingresada es formato nuevo
 	public static boolean esNueva (String matricula) {
 		/*	
-		 * Se verifica el tercer caracter ya que 
-		 * al tener una matrícula "AA123AA"
-		 * el tercer caracter es un número, pero
-		 * en cambio en "AAA123" el tercer caracter
-		 * es una letra, y esto desencadena una
-		 * NumberFormatException.
+		 * Se prueba la existencia de números utilizando
+		 * la excepción NumberFormatException al parsearlo.
+		 * Formato: AA111AA.
 		 */
 		try {
 			Integer.valueOf(matricula.charAt(2));
+			Integer.valueOf(matricula.charAt(3));
+			Integer.valueOf(matricula.charAt(4));
+			return true;
+		}
+		catch (NumberFormatException error) {
+			return false;
+		}
+	}
+
+//	Método para identificar si la matrícula ingresada es formato antiguo 
+	public static boolean esAntigua (String matricula) {
+		/*	
+		 * Se prueba la existencia de números utilizando
+		 * la excepción NumberFormatException al parsearlo.
+		 * Formato: AAA111.
+		 */
+		try {
+			Integer.valueOf(matricula.charAt(3));
+			Integer.valueOf(matricula.charAt(4));
+			Integer.valueOf(matricula.charAt(5));
 			return true;
 		}
 		catch (NumberFormatException error) {
