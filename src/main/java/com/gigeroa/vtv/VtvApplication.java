75c7b8de66e92eaa2077b1d7d126e89d41414f14
Guillermo Gigeroa <guillermo.gigeroa@hotmail.com>
2021-12-13 15:25:32 -0300

    Se migra proyecto a Spring Boot

---
+++ src/main/java/com/gigeroa/vtv/VtvApplication.java
@@ -0,0 +1,13 @@
+package com.gigeroa.vtv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VtvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VtvApplication.class, args);
+	}
+
+}
