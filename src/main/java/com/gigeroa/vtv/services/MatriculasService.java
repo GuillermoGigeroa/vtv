024ac7d726adc1a8989ecd01dcac27b660aa4507
Guillermo Gigeroa <guillermo.gigeroa@hotmail.com>
2021-12-13 09:41:00 -0300

    Se corrigen detalles en los métodos para poder realizar las pruebas

--- src/main/java/com/gigeroa/vtv/services/MatriculasService.java
+++ src/main/java/com/gigeroa/vtv/services/MatriculasService.java
@@ -10,7 +10,7 @@ import com.gigeroa.vtv.repositories.IMatricula;
 public abstract class MatriculasService {
 	
 //	Método para procesar el texto ingresado y verificar si es válido
-	public IMatricula procesarMatricula (String matricula) {
+	public static IMatricula procesarMatricula (String matricula) {
 		IMatricula resultado = null;
 		String matriculaActual = matricula.toUpperCase().trim();
 		if (!(matriculaActual.length() >= 6 && matriculaActual.length() <= 7)) {
@@ -30,9 +30,7 @@ public abstract class MatriculasService {
 		 * Formato: AA111AA.
 		 */
 		try {
-			Integer.valueOf(matricula.charAt(2));
-			Integer.valueOf(matricula.charAt(3));
-			Integer.valueOf(matricula.charAt(4));
+			Integer.parseInt(matricula.substring(2, 5));
 			return true;
 		}
 		catch (NumberFormatException error) {
@@ -48,9 +46,7 @@ public abstract class MatriculasService {
 		 * Formato: AAA111.
 		 */
 		try {
-			Integer.valueOf(matricula.charAt(3));
-			Integer.valueOf(matricula.charAt(4));
-			Integer.valueOf(matricula.charAt(5));
+			Integer.parseInt(matricula.substring(3, 6));
 			return true;
 		}
 		catch (NumberFormatException error) {
