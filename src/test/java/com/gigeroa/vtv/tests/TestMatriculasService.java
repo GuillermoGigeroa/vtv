dc4c03d92bb3849299228d17efd58c2a70ee3733
Guillermo Gigeroa <guillermo.gigeroa@hotmail.com>
2021-12-13 10:24:09 -0300

    Se generan las pruebas con JUnit

--- src/test/java/com/gigeroa/vtv/tests/TestMatriculasService.java
+++ src/test/java/com/gigeroa/vtv/tests/TestMatriculasService.java
@@ -12,44 +12,57 @@ public class TestMatriculasService extends TestCase {
 		patenteAntigua = "AAA123";
 	}
 	
-	public void testEsNueva1 () {
+	public void testEsNueva_Nueva () {
 		escenario();
 		assertTrue(MatriculasService.esNueva(patenteNueva));
 	}
 	
-	public void testEsNueva2 () {
+	public void testEsNueva_Antigua () {
 		escenario();
 		assertFalse(MatriculasService.esNueva(patenteAntigua));
 	}
 	
-	public void testEsAntigua1 () {
+	public void testEsAntigua_Antigua () {
 		escenario();
 		assertTrue(MatriculasService.esAntigua(patenteAntigua));
 	}
 
-	public void testEsAntigua2 () {
+	public void testEsAntigua_Nueva () {
 		escenario();
 		assertFalse(MatriculasService.esAntigua(patenteNueva));
 	}
 	
-	public void testProcesarMatricula1 () {
+	public void testProcesarMatricula_Nueva () {
 		escenario();
 		assertNotNull(MatriculasService.procesarMatricula(patenteNueva));
 	}
 
-	public void testProcesarMatricula2 () {
+	public void testProcesarMatricula_Antigua () {
 		escenario();
 		assertNotNull(MatriculasService.procesarMatricula(patenteAntigua));
 	}
-
-	public void testProcesarMatricula3 () {
+	
+	public void testProcesarMatricula_Errores () {
 		escenario();
 		assertNull(MatriculasService.procesarMatricula("AAA"));
 		assertNull(MatriculasService.procesarMatricula("123"));
 		assertNull(MatriculasService.procesarMatricula("AA5A5JJ"));
-//		assertNull(MatriculasService.procesarMatricula("1AA123")); TODO Revisar este caso. Ver letras
-//		assertNull(MatriculasService.procesarMatricula("AA123A5")); TODO Revisar este caso. Ver letras
+		assertNull(MatriculasService.procesarMatricula("1AA123")); 
+		assertNull(MatriculasService.procesarMatricula("AA123A5"));
 		assertNull(MatriculasService.procesarMatricula("AAA12A"));
 		assertNull(MatriculasService.procesarMatricula("A123AAA"));
+	}
+	
+	public void testEsTexto_Antigua () {
+		escenario();
+		// Formato AAA123
+		assertTrue(MatriculasService.esTexto(patenteAntigua.substring(0,3)));
+	}
+	
+	public void testEsTexto_Nueva () {
+		escenario();
+		// Formato AA123AA
+		assertTrue(MatriculasService.esTexto(patenteNueva.substring(0,2)));
+		assertTrue(MatriculasService.esTexto(patenteNueva.substring(5)));
 	}
 }
