5d79fe6a48619ab26675a43b68dfeab1ba8b7f7e
Guillermo Gigeroa <guillermo.gigeroa@hotmail.com>
2021-12-13 12:12:18 -0300

    Update texto de excepción

--- src/main/java/com/gigeroa/vtv/exceptions/DniInvalido.java
+++ src/main/java/com/gigeroa/vtv/exceptions/DniInvalido.java
@@ -6,7 +6,7 @@ public class DniInvalido extends Exception {
 
 	@Override
 	public String getMessage() {
-		return "El numero de DNI posee letras";
+		return "El dni ingresado no es válido, ingrese números por favor.";
 	}
 
 }
