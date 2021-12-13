5f569cbd5801d96f2b4ca28eeb097e25fe84ad67
Guillermo Gigeroa <guillermo.gigeroa@hotmail.com>
2021-12-13 16:42:40 -0300

    Update pruebas sobre el proyecto

--- src/main/java/com/gigeroa/vtv/services/HomeController.java
+++ src/main/java/com/gigeroa/vtv/services/HomeController.java
@@ -1,5 +1,6 @@
 package com.gigeroa.vtv.services;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,10 +8,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HomeController {
 	
+	@Value ("${index.titulo}")
+	private String titulo;
+	
 	@GetMapping ("/")
 	public String inicio(Model model) {
 		String mensaje = "Mensaje escrito con Thymeleaf";
-		String titulo = "Inicio - Proyecto VTV";
 		model.addAttribute("mensaje", mensaje);
 		model.addAttribute("titulo", titulo);
 		return "index";
