6b77765fd130ee6d092fd4c7265b82db38d506ac
Guillermo Gigeroa <guillermo.gigeroa@hotmail.com>
2021-12-14 16:14:31 -0300

    Se modifica HomeController para manejar los datos de cada página

--- src/main/java/com/gigeroa/vtv/web/HomeController.java
+++ src/main/java/com/gigeroa/vtv/web/HomeController.java
@@ -10,24 +10,25 @@ import com.gigeroa.vtv.entities.*;
 
 @Controller
 public class HomeController {
-	private final String titulo = "Inicio - Proyecto VTV";
+	private String titulo;
 	
 	@GetMapping ("/")
-	public String inicio(Model model) {
+	public String inicio (Model model) {
+		titulo = "Inicio - Proyecto VTV";
 //		Se agrega un título para mostrar.
 		model.addAttribute("titulo", titulo);
 		
 //		Se prueba cargar datos desde la base de datos a una lista - Vehiculos
 		ArrayList<Vehiculo> listaVehiculos = (new DtoVehiculos()).listarVehiculos();
 		model.addAttribute("listaVehiculos", listaVehiculos);
-
-//		Se prueba cargar datos desde la base de datos a una lista - Inspectores
-		ArrayList<Inspector> listaInspectores = (new DtoInspectores()).listarInspectores();
-		model.addAttribute("listaInspectores", listaInspectores);
 		
 //		Se prueba cargar datos desde la base de datos a una lista - Propietarios
 		ArrayList<Propietario> listaPropietarios = (new DtoPropietarios()).listarPropietarios();
 		model.addAttribute("listaPropietarios", listaPropietarios);
+		
+//		Se prueba cargar datos desde la base de datos a una lista - Inspectores
+		ArrayList<Inspector> listaInspectores = (new DtoInspectores()).listarInspectores();
+		model.addAttribute("listaInspectores", listaInspectores);
 		
 //		Se prueba cargar datos desde la base de datos a una lista - Inspecciones
 		ArrayList<Inspeccion> listaInspecciones = (new DtoInspecciones()).listarInspecciones();
@@ -35,4 +36,61 @@ public class HomeController {
 		
 		return "index";
 	}
+	
+	@GetMapping("/listarInspecciones")
+	public String listarInspecciones (Model model) {
+		titulo = "Listar inspecciones";
+		model.addAttribute("titulo",titulo);
+		return "listarInspecciones";
+	}
+
+	@GetMapping("/agregarInspeccion")
+	public String agregarInspecciones (Model model) {
+		titulo = "Agregar inspeccion";
+		model.addAttribute("titulo",titulo);
+		return "agregarInspeccion";
+	}
+
+	@GetMapping("/listarInspectores")
+	public String listarInspectores (Model model) {
+		titulo = "Listar inspectores";
+		model.addAttribute("titulo",titulo);
+		return "listarInspectores";
+	}
+	
+	@GetMapping("/agregarInspector")
+	public String agregarInspector (Model model) {
+		titulo = "Agregar inspector";
+		model.addAttribute("titulo",titulo);
+		return "agregarInspector";
+	}
+
+	@GetMapping("/listarVehiculos")
+	public String listarVehiculos (Model model) {
+		titulo = "Listar vehículos";
+		model.addAttribute("titulo",titulo);
+		return "listarVehiculos";
+	}
+	
+	@GetMapping("/agregarVehiculo")
+	public String agregarVehiculo (Model model) {
+		titulo = "Agregar vehículo";
+		model.addAttribute("titulo",titulo);
+		return "agregarVehiculo";
+	}
+
+	@GetMapping("/listarPropietarios")
+	public String listarPropietarios (Model model) {
+		titulo = "Listar propietarios";
+		model.addAttribute("titulo",titulo);
+		return "listarPropietarios";
+	}
+	
+	@GetMapping("/agregarPropietario")
+	public String agregarPropietarios (Model model) {
+		titulo = "Agregar propietario";
+		model.addAttribute("titulo",titulo);
+		return "agregarPropietario";
+	}
+
 }
