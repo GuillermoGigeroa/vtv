e572a4be01ee905d31a5376eff3c4af360a9e039
Guillermo Gigeroa <guillermo.gigeroa@hotmail.com>
2021-12-15 09:54:11 -0300

    Se limpia el controller, ya fue derivado al resto de controllers

--- src/main/java/com/gigeroa/vtv/web/HomeController.java
+++ src/main/java/com/gigeroa/vtv/web/HomeController.java
@@ -1,11 +1,8 @@
 package com.gigeroa.vtv.web;
 
-import java.util.ArrayList;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import com.gigeroa.vtv.dto.*;
-import com.gigeroa.vtv.entities.*;
 
 @Controller
 public class HomeController {
@@ -16,23 +13,7 @@ public class HomeController {
 //		Se agrega un título para mostrar.
 		titulo = "Inicio - Proyecto VTV";
 		model.addAttribute("titulo", titulo);
-		
-//		Se prueba cargar datos desde la base de datos a una lista - Vehiculos
-		ArrayList<Vehiculo> listaVehiculos = (new DtoVehiculos()).listarVehiculos();
-		model.addAttribute("listaVehiculos", listaVehiculos);
-		
-//		Se prueba cargar datos desde la base de datos a una lista - Propietarios
-		ArrayList<Propietario> listaPropietarios = (new DtoPropietarios()).listarPropietarios();
-		model.addAttribute("listaPropietarios", listaPropietarios);
-		
-//		Se prueba cargar datos desde la base de datos a una lista - Inspectores
-		ArrayList<Inspector> listaInspectores = (new DtoInspectores()).listarInspectores();
-		model.addAttribute("listaInspectores", listaInspectores);
-		
-//		Se prueba cargar datos desde la base de datos a una lista - Inspecciones
-		ArrayList<Inspeccion> listaInspecciones = (new DtoInspecciones()).listarInspecciones();
-		model.addAttribute("listaInspecciones", listaInspecciones);
-		
+
 		return "home/index";
 	}
 
