c2852b0620955cadd0c0fbd3864585fb33e5cfe0
Guillermo Gigeroa <guillermo.gigeroa@hotmail.com>
2021-12-16 11:31:34 -0300

    Cambio en controller, detecta /index. Pruebas con enum

--- src/main/java/com/gigeroa/vtv/web/HomeController.java
+++ src/main/java/com/gigeroa/vtv/web/HomeController.java
@@ -6,14 +6,36 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
-	private String titulo;
+//	Se agrega un título para mostrar en inicio.
+	private final String tituloIndex = "Inicio - Proyecto VTV";
+
+	public enum Marca {
+		FORD("Ford"), FIAT("Fiat"), TOYOTA("Toyota"), VOLKSWAGEN("Volkswagen");
+		private String nombre;
+		
+		private Marca (String nombre) {
+			this.nombre = nombre;
+		}
+		
+		public String getNombre() {
+			return nombre;
+		}
+	}
 	
 	@GetMapping ("/")
 	public String inicio (Model model) {
-//		Se agrega un título para mostrar.
-		titulo = "Inicio - Proyecto VTV";
-		model.addAttribute("titulo", titulo);
+		model.addAttribute("titulo", tituloIndex);
 
+//		Pruebas de enum
+		model.addAttribute("marcas",Marca.values());
+		
+		return "home/index";
+	}
+
+	@GetMapping ("/index")
+	public String index (Model model) {
+		model.addAttribute("titulo", tituloIndex);
+		
 		return "home/index";
 	}
 
