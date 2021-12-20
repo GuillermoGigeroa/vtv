c4e7b193093c642f00d8f9967b9782599b57ea9c
Guillermo Gigeroa <guillermo.gigeroa@hotmail.com>
2021-12-20 11:43:47 -0300

    Lista de marca y modelo finalizada

--- src/main/java/com/gigeroa/vtv/web/HomeController.java
+++ src/main/java/com/gigeroa/vtv/web/HomeController.java
@@ -3,10 +3,10 @@ package com.gigeroa.vtv.web;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import com.gigeroa.vtv.dto.DtoVehiculos;
 import com.gigeroa.vtv.entities.MarcaVehiculo;
+import com.gigeroa.vtv.entities.ModeloVehiculo;
 
 @Controller
 public class HomeController {
@@ -26,28 +26,50 @@ public class HomeController {
 		return inicioGet(model);
 	}
 
-//	Mapeo de la sección /seleccion para continuar con la selección de la marca y modelo en GET
-	@GetMapping ("/seleccion")
-	public String seleccion (Model model) {
+//	Mapeo de la sección /seleccionMarca para continuar con la selección de la marca
+	@GetMapping ("/seleccionMarca")
+	public String seleccionMarca (Model model, @RequestParam int ID) {
 		titulo(model);
 		listarMarcas(model);
-		marcaNueva(model);
+		listarModelos(model, ID);
+		marcaSelecionada(model, ID);
+		modeloNuevo(model);
 		return "home/index";
 	}
 
-//	Mapeo de la sección /seleccion para continuar con la selección de la marca y modelo en POST
-	@PostMapping ("/seleccion")
-	public String seleccion (Model model, @RequestParam int ID) {
+//	Mapeo de la sección /seleccionModelo para continuar con la selección del modelo
+	@GetMapping ("/seleccionModelo")
+	public String seleccion (Model model, @RequestParam int ID, @RequestParam int idMarca) {
 		titulo(model);
 		listarMarcas(model);
-		marcaSelecionada(model, ID);
-		return "home/index";
+		listarModelos(model, idMarca);
+		marcaSelecionada(model, idMarca);
+		if (modeloSeleccionado(model, idMarca, ID)) {
+			return "home/index";
+		}
+		else {
+			return "redirect:/errorID";
+		}
 	}
 	
 	public void marcaNueva(Model model) {
 		model.addAttribute("marcaVehiculo",new MarcaVehiculo());
 	}
-	
+
+	public void modeloNuevo(Model model) {
+		model.addAttribute("modeloVehiculo",new ModeloVehiculo());
+	}
+
+	public boolean modeloSeleccionado(Model model,int IDMarca, int ID) {
+		for (ModeloVehiculo modelo : (new DtoVehiculos()).listarModelos(IDMarca)) {
+			if (modelo.getID() == ID) {
+				model.addAttribute("modeloVehiculo",modelo);
+				return true;
+			}
+		}
+		return false;
+	}
+
 	public void marcaSelecionada(Model model, int ID) {
 		model.addAttribute("marcaVehiculo",(new DtoVehiculos()).listarMarcas().get(ID-1));
 	}
@@ -58,5 +80,9 @@ public class HomeController {
 
 	public void listarMarcas(Model model) {
 		model.addAttribute("listaMarcas",(new DtoVehiculos()).listarMarcas());
+	}
+
+	public void listarModelos(Model model, int IDMarca) {
+		model.addAttribute("listaModelos",(new DtoVehiculos()).listarModelos(IDMarca));
 	}
 }
