f703b54941c5f8f2b97052dfb0939b56fd74c537
Guillermo Gigeroa <guillermo.gigeroa@hotmail.com>
2021-12-20 17:54:52 -0300

    Se migra Dao con JPA

---
+++ src/main/java/com/gigeroa/vtv/dao/DaoVehiculos2.java
@@ -0,0 +1,8 @@
+package com.gigeroa.vtv.dao;
+
+import org.springframework.data.repository.CrudRepository;
+import com.gigeroa.vtv.entities.Vehiculo;
+
+public interface DaoVehiculos2 extends CrudRepository<Vehiculo, Integer>{
+
+}
