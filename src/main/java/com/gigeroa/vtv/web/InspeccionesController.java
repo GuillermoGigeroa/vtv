e34c686fafb9a493c1f75bf5f64d03731b3aebd4
Guillermo Gigeroa <guillermo.gigeroa@hotmail.com>
2021-12-21 14:46:18 -0300

    Se editar con el ControllersService

--- src/main/java/com/gigeroa/vtv/web/InspeccionesController.java
+++ src/main/java/com/gigeroa/vtv/web/InspeccionesController.java
@@ -5,29 +5,24 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import com.gigeroa.vtv.dto.DtoInspeccionesImpl;
+import com.gigeroa.vtv.services.ControllersService;
 
 @Controller
 public class InspeccionesController {
-	private String titulo;
-	
 	@Autowired
 	DtoInspeccionesImpl dto;
 	
 	@GetMapping ("/listarInspecciones")
 	public String listarInspecciones (Model model) {
-		titulo = "Lista de inspecciones";
-		model.addAttribute("titulo",titulo);
-		
+		ControllersService.setTitulo(model, "Lista de inspecciones");
 //		Se trae lista de inspecciones
 		model.addAttribute("listaInspecciones", dto.listar());
-		
 		return "inspecciones/listarInspecciones";
 	}
 	
 	@GetMapping("/agregarInspeccion")
 	public String agregarInspecciones (Model model) {
-		titulo = "Agregar inspeccion";
-		model.addAttribute("titulo",titulo);
+		ControllersService.setTitulo(model, "Agregar inspeccion");
 		return "inspecciones/agregarInspeccion";
 	}
 }
