b89cacf1450f0fe3f81dfa6d8bad69facef59037
Guillermo Gigeroa <guillermo.gigeroa@hotmail.com>
2021-12-24 11:27:24 -0300

    Se actualiza VehiculosController

--- src/main/java/com/gigeroa/vtv/web/VehiculosController.java
+++ src/main/java/com/gigeroa/vtv/web/VehiculosController.java
@@ -6,6 +6,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import com.gigeroa.vtv.dto.*;
 import com.gigeroa.vtv.entities.*;
@@ -29,7 +31,7 @@ public class VehiculosController {
 	@Autowired
 	private DtoModelosImpl dtoModelos;
 	
-	@GetMapping("/listarVehiculos")
+	@RequestMapping(value = {"/listarVehiculos"}, method = {RequestMethod.GET, RequestMethod.POST})
 	public String listarVehiculos (Model model) {
 		ControllersService.setTitulo(model, "Lista de vehículos");
 //		Se trae lista de vehiculos
@@ -37,18 +39,23 @@ public class VehiculosController {
 		return listarVehiculos;
 	}
 
-	@GetMapping("/listarVehiculos/{agregar}")
-	public String listarVehiculos2 (Model model, @PathVariable int agregar) {
-		if (agregar != 0) {
-			model.addAttribute("mensaje", "Para agregar una inspección debe presionar este botón en el vehiculo");
+	@RequestMapping(value = {"/listarVehiculos/{agregar}"}, method = {RequestMethod.GET, RequestMethod.POST})
+	public String listarVehiculos2 (Model model, @PathVariable (required = false) Integer agregar) {
+		if (agregar == null) {
+			return "redirect:/listarVehiculos";
 		}
 		ControllersService.setTitulo(model, "Lista de vehículos");
+		
+//		Si un valor numérico entra por pathvariable entonces aparece el mensaje
+		model.addAttribute("mensaje", "Para agregar una inspección debe presionar este botón en el vehiculo");
+		
 //		Se trae lista de vehiculos
 		model.addAttribute("listaVehiculos", dtoPyV.listar());
+		
 		return listarVehiculos;
 	}
 
-	@GetMapping("/agregarVehiculo")
+	@RequestMapping(value = {"/agregarVehiculo", "/agregarVehiculo/"}, method = {RequestMethod.GET, RequestMethod.POST})
 	public String agregarVehiculo (Model model) {
 		ControllersService.setTitulo(model, "Agregar vehículo");
 		listarMarcas(model);
@@ -56,24 +63,17 @@ public class VehiculosController {
 		return agregarVehiculo;
 	}
 	
-	@GetMapping("/seleccionMarca")
-	public String marca (Model model) {
-		return agregarVehiculo(model);
-	}
-
-	@GetMapping("/seleccionModelo")
-	public String modelo (Model model) {
-		return agregarVehiculo(model);
-	}
-
 	@GetMapping("/seleccionPropietario")
 	public String propietario (Model model) {
 		return agregarVehiculo(model);
 	}
 
 //	Mapeo de la sección /seleccionMarca para continuar con la selección de la marca
-	@PostMapping ("/seleccionMarca")
-	public String seleccionMarca (Model model, @RequestParam int ID) {
+	@RequestMapping(value = {"/seleccionMarca", "/seleccionMarca/"}, method = {RequestMethod.GET, RequestMethod.POST})
+	public String seleccionMarca (Model model, @RequestParam (required = false) Integer ID) {
+		if (ID == null) {
+			return "redirect:/agregarVehiculo";
+		}
 		ControllersService.setTitulo(model, "Agregar vehículo");
 		listarMarcas(model);
 		listarModelos(model, ID);
@@ -83,39 +83,66 @@ public class VehiculosController {
 	}
 
 //	Mapeo de la sección /seleccionModelo para continuar con la selección del modelo
-	@PostMapping ("/seleccionModelo")
-	public String seleccion (Model model, @RequestParam int ID, @RequestParam int idMarca) throws DniInvalido {
+	@RequestMapping(value = {"/seleccionModelo", "/seleccionModelo/"}, method = {RequestMethod.GET, RequestMethod.POST})
+	public String seleccion (Model model, @RequestParam (required = false) Integer ID, @RequestParam (required = false) Integer idMarca) throws DniInvalido {
+		if (ID == null | idMarca == null) {
+			return "redirect:/agregarVehiculo";
+		}
 		ControllersService.setTitulo(model, "Agregar vehículo");
 		listarMarcas(model);
 		listarModelos(model, idMarca);
 		listarPropietarios(model);
 		propietarioNuevo(model);
 		if (!marcaSelecionada(model, idMarca)) {
-			return "redirect:/errorURL";
+			return "redirect:/agregarVehiculo?marcaInvalida";
 		}
 		if (!modeloSeleccionado(model, idMarca, ID)) {
-			return "redirect:/errorURL";
+			return "redirect:/agregarVehiculo?modeloInvalido";
 		}
 		return agregarVehiculo;
 	}
 	
 //	Mapeo de la sección /seleccionPropietario para continuar con la selección del propietario
-	@PostMapping ("/seleccionPropietario")
-	public String seleccionPropietario (Model model, @RequestParam int idModelo, @RequestParam int idMarca, @RequestParam String nombre) {
+	@RequestMapping(value = {"/seleccionPropietario", "/seleccionPropietario/"}, method = {RequestMethod.GET, RequestMethod.POST})
+	public String seleccionPropietario (Model model,
+			@RequestParam (required = false) Integer idModelo,
+			@RequestParam (required = false) Integer idMarca,
+			@RequestParam (required = false) String nombre) {
+		if (idModelo == null | idMarca == null | nombre == null) {
+			return "redirect:/agregarVehiculo";
+		}
 		ControllersService.setTitulo(model, "Agregar vehículo");
 		listarMarcas(model);
 		listarModelos(model, idMarca);
 		listarPropietarios(model);
 		if (!marcaSelecionada(model, idMarca)) {
-			return "redirect:/errorURL";
+			return "redirect:/agregarVehiculo?marcaInvalida";
 		}
 		if (!modeloSeleccionado(model, idMarca, idModelo)) {
-			return "redirect:/errorURL";
+			return "redirect:/agregarVehiculo?modeloInvalido";
 		}
 		if (!propietarioSeleccionado(model, nombre)) {
-			return "redirect:/errorURL";
+			return "redirect:/agregarVehiculo?propietarioInvalido";
 		}
 		return agregarVehiculo;
+	}
+	
+	@RequestMapping(value = {"/editarVehiculo", "/editarVehiculo/{idVehiculo}"}, method = {RequestMethod.GET, RequestMethod.POST})
+	public String editar (Model model, @PathVariable (required = false) Integer idVehiculo) {
+		if (idVehiculo == null) {
+			return "redirect:/listarVehiculos";
+		}
+		ControllersService.setTitulo(model,"En desarrollo - ID Vehiculo: "+idVehiculo);
+		return "home/index";
+	}
+
+	@RequestMapping(value = {"/eliminarVehiculo", "/eliminarVehiculo/{idVehiculo}"}, method = {RequestMethod.GET, RequestMethod.POST})
+	public String eliminar (Model model, @PathVariable (required = false) Integer idVehiculo) {
+		if (idVehiculo == null) {
+			return "redirect:/listarVehiculos";
+		}
+		ControllersService.setTitulo(model,"En desarrollo - ID Vehiculo: "+idVehiculo);
+		return "home/index";
 	}
 	
 	private void listarPropietarios(Model model) {
@@ -174,17 +201,5 @@ public class VehiculosController {
 
 	private void listarModelos(Model model, int IDMarca) {
 		model.addAttribute("listaModelos", dtoModelos.listarModelosPorMarca(IDMarca));
-	}
-	
-	@GetMapping("/editarVehiculo/{idVehiculo}")
-	public String editar (Model model, @PathVariable int idVehiculo) {
-		ControllersService.setTitulo(model,"En desarrollo - ID Vehiculo: "+idVehiculo);
-		return "home/index";
-	}
-
-	@GetMapping("/eliminarVehiculo/{idVehiculo}")
-	public String eliminar (Model model, @PathVariable int idVehiculo) {
-		ControllersService.setTitulo(model,"En desarrollo - ID Vehiculo: "+idVehiculo);
-		return "home/index";
 	}
 }
