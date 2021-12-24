46586591f4ff674292bf13c2ba79ed7ff9c96b11
Guillermo Gigeroa <guillermo.gigeroa@hotmail.com>
2021-12-24 10:48:42 -0300

    Se actualiza PropietariosController

--- src/main/java/com/gigeroa/vtv/web/PropietariosController.java
+++ src/main/java/com/gigeroa/vtv/web/PropietariosController.java
@@ -3,9 +3,9 @@ package com.gigeroa.vtv.web;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import com.gigeroa.vtv.dto.DtoPropietario_y_Vehiculo;
 import com.gigeroa.vtv.dto.DtoPropietariosImpl;
 import com.gigeroa.vtv.entities.Propietario;
@@ -24,40 +24,42 @@ public class PropietariosController {
 	@Autowired
 	DtoPropietario_y_Vehiculo dtoPyV;
 	
-	@GetMapping("/listaPropietarios")
+	@RequestMapping (value = {"/listaPropietarios", "/listaPropietarios/"}, method = {RequestMethod.GET, RequestMethod.POST})
 	public String listaPropietarios (Model model) {
 		ControllersService.setTitulo(model, "Lista de propietarios");
 		listarPropietarios(model);
 		return listaPropietarios;
 	}
 	
-	@GetMapping("/agregarPropietario")
+	@RequestMapping (value = {"/agregarPropietario", "/agregarPropietario/"}, method = {RequestMethod.GET, RequestMethod.POST})
 	public String agregarPropietarios (Model model) throws DniInvalido {
 		ControllersService.setTitulo(model, "Agregar propietario");
 		model.addAttribute("propietario",new Propietario());
 		return agregarPropietario;
 	}
 
-	@GetMapping("/altaPropietario")
+	@RequestMapping (value = {"/altaPropietario","/altaPropietario/"}, method = {RequestMethod.GET, RequestMethod.POST})
 	public String altaPropietarios (Model model, Propietario propietario) {
 		ControllersService.setTitulo(model, "Agregar propietario");
 		return agregarPropietario;
 	}
 	
-//	Método para listar los propietarios
-	private void listarPropietarios(Model model) {
-		model.addAttribute("listaPropietarios", dtoPyV.listar());
-	}
-	
-	@GetMapping("/editarPropietario/{dni}")
-	public String editar (Model model, @PathVariable int dni) {
+	@RequestMapping (value = {"/editarPropietario","/editarPropietario/{dni}"}, method = {RequestMethod.GET, RequestMethod.POST})
+	public String editar (Model model, @PathVariable (required = false) Integer dni) {
+		if (dni == null) {return "redirect:/listaPropietarios";}
 		ControllersService.setTitulo(model,"En desarrollo - DNI: "+dni);
 		return "home/index";
 	}
 
-	@GetMapping("/eliminarPropietario/{dni}")
-	public String eliminar (Model model, @PathVariable int dni) {
+	@RequestMapping (value = {"/eliminarPropietario","/eliminarPropietario/{dni}"}, method = {RequestMethod.GET, RequestMethod.POST})
+	public String eliminar (Model model, @PathVariable (required = false) Integer dni) {
+		if (dni == null) {return "redirect:/listaPropietarios";}
 		ControllersService.setTitulo(model,"En desarrollo - DNI: "+dni);
 		return "home/index";
+	}
+	
+//	Método para listar los propietarios
+	private void listarPropietarios(Model model) {
+		model.addAttribute("listaPropietarios", dtoPyV.listar());
 	}
 }
