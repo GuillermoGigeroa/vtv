7953a295b1c9c2b15f7be5b842965d1a82f942d5
Guillermo Gigeroa <guillermo.gigeroa@hotmail.com>
2021-12-24 10:34:09 -0300

    Se actualiza InspectoresController

--- src/main/java/com/gigeroa/vtv/web/InspectoresController.java
+++ src/main/java/com/gigeroa/vtv/web/InspectoresController.java
@@ -3,9 +3,9 @@ package com.gigeroa.vtv.web;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import com.gigeroa.vtv.dto.DtoInspectoresImpl;
 import com.gigeroa.vtv.entities.Inspector;
 import com.gigeroa.vtv.services.ControllersService;
@@ -15,36 +15,41 @@ public class InspectoresController {
 	@Autowired
 	DtoInspectoresImpl dto;
 	
-	@GetMapping("/listarInspectores")
+	@RequestMapping (value = {"/listarInspectores", "/listarInspectores/"}, method = {RequestMethod.GET, RequestMethod.POST})
 	public String listarInspectores (Model model) {
 		ControllersService.setTitulo(model, "Lista de inspectores");
 //		Se trae lista de inspectores
 		model.addAttribute("listaInspectores", dto.listar());
-		
 		return "inspectores/listarInspectores";
 	}
 	
-	@GetMapping("/agregarInspector")
+	@RequestMapping (value = {"/agregarInspector", "/agregarInspector/"}, method = {RequestMethod.GET, RequestMethod.POST})
 	public String agregarInspector (Model model) {
 		ControllersService.setTitulo(model, "Alta de inspector");
 		model.addAttribute("inspector",new Inspector());
 		return "inspectores/agregarInspector";
 	}
 
-	@GetMapping("/altaInspector")
+	@RequestMapping (value = {"/altaInspector","/altaInspector/"}, method = {RequestMethod.GET, RequestMethod.POST})
 	public String agregarInspector2 (Model model, Inspector inspector) {
 		ControllersService.setTitulo(model, "Alta de inspector");
 		return "inspectores/agregarInspector";
 	}
 	
-	@GetMapping("/editarInspector/{legajo}")
-	public String editar (Model model, @PathVariable int legajo) {
+	@RequestMapping (value = {"/editarInspector","/editarInspector/{legajo}"}, method = {RequestMethod.GET, RequestMethod.POST})
+	public String editar (Model model, @PathVariable (required = false) Integer legajo) {
+		if (legajo == null) {
+			return "redirect:/listarInspectores";
+		}
 		ControllersService.setTitulo(model,"En desarrollo - Legajo: "+legajo);
 		return "home/index";
 	}
 
-	@GetMapping("/eliminarInspector/{legajo}")
-	public String eliminar (Model model, @PathVariable int legajo) {
+	@RequestMapping (value = {"/eliminarInspector","/eliminarInspector/{legajo}"}, method = {RequestMethod.GET, RequestMethod.POST})
+	public String eliminar (Model model, @PathVariable (required = false) Integer legajo) {
+		if (legajo == null) {
+			return "redirect:/listarInspectores";
+		}
 		ControllersService.setTitulo(model,"En desarrollo - Legajo: "+legajo);
 		return "home/index";
 	}
