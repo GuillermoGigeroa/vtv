77e6a16dcef82aa377a1b36d4a8fd68574a02f1d
Guillermo Gigeroa <guillermo.gigeroa@hotmail.com>
2021-12-27 12:47:39 -0300

    Update pruebas REST

--- src/main/java/com/gigeroa/vtv/rest/InspeccionesRest.java
+++ src/main/java/com/gigeroa/vtv/rest/InspeccionesRest.java
@@ -1,13 +1,14 @@
 package com.gigeroa.vtv.rest;
 
 import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.gigeroa.vtv.dto.DtoInspeccion_y_Vehiculo;
+import com.gigeroa.vtv.dto.DtoInspeccionesImpl;
 import com.gigeroa.vtv.entities.Inspeccion_y_Vehiculo;
 
 @RestController
@@ -15,10 +16,18 @@ import com.gigeroa.vtv.entities.Inspeccion_y_Vehiculo;
 public class InspeccionesRest {
 	
 	@Autowired
-	DtoInspeccion_y_Vehiculo dto;
+	DtoInspeccion_y_Vehiculo dtoIyV;
+
+	@Autowired
+	DtoInspeccionesImpl dtoInspecciones;
 	
-	@GetMapping ("/listar")
+	@GetMapping
 	public List<Inspeccion_y_Vehiculo> listar (){
-		return dto.listar();
+		return dtoIyV.listar();
+	}
+	
+	@PostMapping
+	public void agregar(@RequestBody Inspeccion_y_Vehiculo iyv) {
+		dtoInspecciones.guardar(iyv.getInspeccion(), iyv.getVehiculo().getID());
 	}
 }
